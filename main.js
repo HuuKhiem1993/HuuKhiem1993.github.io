1ab21daa649f0e51155365344f1e36298fd83fc1
HuuKhiem <huukhiem1909@gmail.com>
2024-04-08 15:56:40 +0700

    50%

---
+++ main.js
@@ -0,0 +1,21 @@
+window.addEventListener('resize', function() {
+    const screenWidth = screen.width;
+    const screenHeight = screen.height;
+    const bg_left = this.document.getElementsByClassName('bg-left')
+    const bg_right = this.document.getElementsByClassName('bg-right')
+
+    if (screenWidth<=1000){
+        console.log(screenWidth, screenHeight)
+        bg_left[0].classList.remove('col-3');
+        bg_right[0].classList.remove('col-7');
+        console.log(bg_left)
+    }
+    else{
+        bg_left[0].classList.add('col-3');
+        bg_right[0].classList.add('col-7');
+    }
+
+    
+
+    
+});
