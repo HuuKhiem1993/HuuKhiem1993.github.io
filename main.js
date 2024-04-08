aefa5bf71c7ef36930a22e498e376d352a013b03
HuuKhiem <huukhiem1909@gmail.com>
2024-04-08 16:26:38 +0700

    55%

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-if (screenWidth<=1000){
-    console.log(screenWidth, screenHeight)
-    bg_left[0].classList.remove('col-3');
-    bg_right[0].classList.remove('col-7');
-    console.log(bg_left)
-}
-window.addEventListener('resize', function() {
-    const screenWidth = screen.width;
-    const screenHeight = screen.height;
+
+function responsive(){
+    let screenWidth = screen.width;
+    let screenHeight = screen.height;
     const bg_left = this.document.getElementsByClassName('bg-left')
     const bg_right = this.document.getElementsByClassName('bg-right')
 
@@ -14,14 +9,33 @@ window.addEventListener('resize', function() {
         console.log(screenWidth, screenHeight)
         bg_left[0].classList.remove('col-3');
         bg_right[0].classList.remove('col-7');
+        bg_left[0].classList.add('padding-30')
         console.log(bg_left)
     }
-    else{
-        bg_left[0].classList.add('col-3');
-        bg_right[0].classList.add('col-7');
-    }
+    window.addEventListener('resize', function() {
+        screenWidth = screen.width;
+        screenHeight = screen.height;
+        
 
-    
+        if (screenWidth<=1000){
+            console.log(screenWidth, screenHeight)
+            bg_left[0].classList.remove('col-3');
+            bg_right[0].classList.remove('col-7');
+            bg_left[0].classList.add('padding-30')
+            console.log(bg_left)
+        }
+        else{
+            bg_left[0].classList.add('col-3');
+            bg_right[0].classList.add('col-7');
+            bg_left[0].classList.remove('padding-30')
+        }
 
-    
-});
+        
+
+        
+    });
+
+}
+
+
+
